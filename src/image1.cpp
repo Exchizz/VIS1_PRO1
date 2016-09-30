f256cdffc0a41c4262066a5659996d6abbe7b1f9
Mathias Neerup <manee12@student.sdu.dk>
2016-09-30 11:51:14 +0200

    Median blur works

--- src/image1.cpp
+++ src/image1.cpp
@@ -6,8 +6,11 @@ using namespace cv;
 
 int main(int argc, char** argv )
 {
-    Mat image;
+    Mat image, dst;
     image = cv::imread("ImagesForStudents/Image2.png", CV_LOAD_IMAGE_GRAYSCALE);
+
+
+    medianBlur ( image, dst, 11 );
 
     if ( !image.data )
     {
@@ -15,8 +18,8 @@ int main(int argc, char** argv )
         return -1;
     }
 
-    namedWindow("Display Image", CV_WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    namedWindow("Display Image", CV_WINDOW_NORMAL );
+    imshow("Display Image", dst);
 
     waitKey(0);
 
