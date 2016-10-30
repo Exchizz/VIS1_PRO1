b187b466629b4c11322bda7d7662b39c2779cd6e
Mathias Neerup <manee12@student.sdu.dk>
2016-10-30 14:48:35 +0100

    Image3 looks alright

--- src/image3.cpp
+++ src/image3.cpp
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "functions.hpp"
 
 using namespace cv;
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-
     Mat image;
-    image = imread( argv[1], 1 );
+    image = imread( "ImagesForStudents/Image3.png", CV_LOAD_IMAGE_GRAYSCALE );
 
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", CV_WINDOW_AUTOSIZE );
+    namedWindow("Display Image", CV_WINDOW_NORMAL );
     imshow("Display Image", image);
 
-    waitKey(0);
+
+
+    Mat image_noise_crop = image(Rect(Point(1000, 1300), Point(1100, 1400)));
+    SaveImage(image_noise_crop, "image_noise_crop_before", false);
+
+
+    medianBlur(image,image, 7);
+
+    cv::namedWindow("Output image",CV_WINDOW_NORMAL);
+    cv::imshow("Output image", image);
+
+
+    // Generate kernel using:
+    cv::Mat_<float> kernel(3,3);
+    kernel << 0,  1, 0,
+              1, -4, 1,
+              0,  1, 0;
+
+    SharpenFilter(image,kernel);
+    cv::namedWindow("Output sharpened",CV_WINDOW_NORMAL);
+    cv::imshow("Output sharpened", image);
+
+    cv::waitKey(0);
 
     return 0;
 }
